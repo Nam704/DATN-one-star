d8d3851f8c1ee919fd4c536b83fee15d8f48f62d
Nam704 <daovannam2k4@gmail.com>
2025-01-08 18:02:15 +0700

    fix composer

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
 
     use HasFactory, SoftDeletes;
 
-    protected $fillable = [ 
+    protected $fillable = [
         'name',
         'id_brand',
         'id_category',
@@ -23,20 +23,22 @@ class Product extends Model
         'deleted_at',
     ];
 
-    public function Category(){
-        return $this->belongsTo(Category::class,'id_category');
+    public function Category()
+    {
+        return $this->belongsTo(Category::class, 'id_category');
     }
 
-    public function Brand(){
-        return $this->belongsTo(Brand::class,'id_brand');
+    public function Brand()
+    {
+        return $this->belongsTo(Brand::class, 'id_brand');
 
-    //     public static function scopeList($query, $idProduct)
-    // {
-    //     return $query->select('suppliers.id', 'suppliers.name', 'suppliers.status', 'products.name as product_name')  // Thêm cột 'product_name' từ bảng 'products'
-    //         ->join('products', 'suppliers.product_id', '=', 'products.id')  // Thực hiện JOIN giữa 'suppliers' và 'products'
-    //         ->where('products.id', $idProduct)  // Lọc theo 'idProduct'
-    //         ->latest('suppliers.id');  // Sắp xếp theo 'id' của bảng 'suppliers'
-    // }
+        //     public static function scopeList($query, $idProduct)
+        // {
+        //     return $query->select('suppliers.id', 'suppliers.name', 'suppliers.status', 'products.name as product_name')  // Thêm cột 'product_name' từ bảng 'products'
+        //         ->join('products', 'suppliers.product_id', '=', 'products.id')  // Thực hiện JOIN giữa 'suppliers' và 'products'
+        //         ->where('products.id', $idProduct)  // Lọc theo 'idProduct'
+        //         ->latest('suppliers.id');  // Sắp xếp theo 'id' của bảng 'suppliers'
+    }
     public static function scopeList($query)
     {
         return $query->select('id', 'name', 'status')
@@ -49,6 +51,5 @@ class Product extends Model
     public function variants()
     {
         return $this->hasMany(Product_variant::class, 'id_product');
-
     }
 }
