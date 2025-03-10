02f7409ea2df4c0ebcc16b92a4cea6d3ee66ec57
Nam704 <daovannam2k4@gmail.com>
2025-03-10 23:44:43 +0700

    update migrate table voucher

--- database/migrations/2025_02_21_170103_create_vouchers_table.php
+++ database/migrations/2025_02_21_170103_create_vouchers_table.php
@@ -16,11 +16,17 @@ return new class extends Migration
             $table->string('name');
             $table->string('code')->unique();
             $table->text('description')->nullable();
+            $table->string('type'); // Loại giảm giá
             $table->decimal('discount_amount', 10, 2);
+            $table->decimal('max_discount_amount', 10, 2)->nullable(); // Giới hạn giảm giá tối đa
             $table->integer('quantity');
+            $table->integer('user_limit')->default(1); // Số lần tối đa cho mỗi user
+            $table->integer('total_usage')->default(0); // Tổng số lần đã sử dụng
             $table->dateTime('start_date');
             $table->dateTime('end_date');
             $table->decimal('min_amount', 10, 2);
+            $table->string('status')->default('active'); // Trạng thái
+            $table->string('applies_to')->nullable(); // Áp dụng cho danh mục/sản phẩm cụ thể
             $table->timestamps();
         });
     }
