90e5b0bf5a3c1bc44170e94663ff80e999333ccf
Nam704 <daovannam2k4@gmail.com>
2025-03-17 15:52:11 +0700

    Cancel order star, paginate client order success

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             AddressSeeder::class,
             OrderStatusSeeder::class,
+            OrderCancellationReasonSeeder::class
         ]);
     }
 }
