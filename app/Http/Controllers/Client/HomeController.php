7ccb938e9c2193d8fa8d1413befcf66267e4e219
datntph37288 <tiendat0986@gmail.com>
2025-04-01 22:39:06 +0700

    sp gợi ý

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\Cart_details;
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\Product_variant;
 use App\Services\CategoryService;
 use App\Services\ProductService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -22,6 +27,53 @@ class HomeController extends Controller
     {
         $categories = $this->categoryService->getCategories();
         // return ($categories);
-        return view('client.index', compact('categories'));
+        $recommendedProducts = $this->getRecommendedProducts();
+        return view('client.index', compact('categories','recommendedProducts'));
+    }
+
+    private function getRecommendedProducts($limit = 10)
+    {
+        if (Auth::check()) {
+            $userId = Auth::id();
+            $userCart = Cart::where('id_user', $userId)->first();
+
+            if ($userCart) {
+                $cartVariants = Cart_details::where('id_cart', $userCart->id)
+                    ->pluck('id_variant')
+                    ->toArray();
+
+                if (!empty($cartVariants)) {
+                    $productsFromVariants = Product_variant::whereIn('id', $cartVariants)
+                        ->pluck('id_product')
+                        ->toArray();
+                    $cartProducts = Product::whereIn('id', $productsFromVariants)->get();
+
+                    if ($cartProducts->count() > 0) {
+                        $categoryIds = [];
+
+                        foreach ($cartProducts as $product) {
+                            $categoryIds[] = $product->id_category;
+                        }
+
+                        $recommendedQuery = Product::whereIn('id_category', $categoryIds)
+                            ->whereNotIn('id', $productsFromVariants);
+
+                        $recommendedProducts = $recommendedQuery->limit($limit)->get();
+                        if ($recommendedProducts->count() < $limit) {
+                            $existingIds = $recommendedProducts->pluck('id')->toArray();
+                            $additionalProducts = Product::whereNotIn('id', array_merge($existingIds, $productsFromVariants))
+                                ->latest()
+                                ->limit($limit - $recommendedProducts->count())
+                                ->get();
+
+                            return $recommendedProducts->concat($additionalProducts);
+                        }
+
+                        return $recommendedProducts;
+                    }
+                }
+            }
+        }
+        return Product::latest()->limit($limit)->get();
     }
 }
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -10,12 +10,12 @@
 
     <section class="product_area mb-50">
         <div class="container">
+            <!-- Phần Our Products -->
             <div class="row">
                 <div class="col-12">
                     <div class="section_title">
-                        <h2><span> <strong>Our</strong>Products</span></h2>
+                        <h2><span> <strong>Our</strong> Products</span></h2>
                         <ul class="product_tab_button nav" role="tablist" id="nav-tab">
-
                             @foreach ($categories as $index => $item)
                                 @if ($item->id_parent == 0)
                                     <li>
@@ -27,7 +27,6 @@
                                     </li>
                                 @endif
                             @endforeach
-
                         </ul>
                     </div>
                 </div>
@@ -44,30 +43,39 @@
                                 @foreach ($item->products as $product)
                                     <div class="single_product">
                                         <div class="product_name">
-                                            <h3><a
-                                                    href="{{ route('client.products.detail', $product->id) }}">{{ $product->name }}</a>
+                                            <h3>
+                                                <a href="{{ route('client.products.detail', $product->id) }}">
+                                                    {{ $product->name }}
+                                                </a>
                                             </h3>
                                             <p class="manufacture_product"><a href="#">Accessories</a></p>
                                         </div>
                                         <div class="product_thumb">
                                             <a class="primary_img"
-                                                href="{{ route('client.products.detail', $product->id) }}"><img
-                                                    src="{{ asset($product->image_primary) }}" alt=""></a>
-
+                                                href="{{ route('client.products.detail', $product->id) }}">
+                                                <img src="{{ asset($product->image_primary) }}" alt="">
+                                            </a>
                                             <div class="label_product">
                                                 <span class="label_sale">-57%</span>
                                             </div>
-
                                             <div class="action_links">
                                                 <ul>
-                                                    <li class="quick_button"><a href="#" data-bs-toggle="modal"
-                                                            data-bs-target="#modal_box" title="quick view"> <span
-                                                                class="lnr lnr-magnifier"></span></a></li>
-                                                    <li class="wishlist"><a href="wishlist.html"
-                                                            title="Add to Wishlist"><span class="lnr lnr-heart"></span></a>
+                                                    <li class="quick_button">
+                                                        <a href="#" data-bs-toggle="modal" data-bs-target="#modal_box"
+                                                            title="quick view">
+                                                            <span class="lnr lnr-magnifier"></span>
+                                                        </a>
                                                     </li>
-                                                    <li class="compare"><a href="compare.html" title="compare"><span
-                                                                class="lnr lnr-sync"></span></a></li>
+                                                    <li class="wishlist">
+                                                        <a href="wishlist.html" title="Add to Wishlist">
+                                                            <span class="lnr lnr-heart"></span>
+                                                        </a>
+                                                    </li>
+                                                    <li class="compare">
+                                                        <a href="compare.html" title="compare">
+                                                            <span class="lnr lnr-sync"></span>
+                                                        </a>
+                                                    </li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -84,25 +92,105 @@
                                             <div class="product_footer d-flex align-items-center">
                                                 <div class="price_box">
                                                     <span class="regular_price"> {{ $product->min_price }}</span>
-
                                                 </div>
-                                                {{-- <div class="add_to_cart">
-                                    <a href="cart.html" title="add to cart"><span class="lnr lnr-cart"></span></a>
-                                </div> --}}
                                             </div>
                                         </div>
                                     </div>
                                 @endforeach
-
                             </div>
                         </div>
                     @endif
                 @endforeach
+            </div>
 
+            <!-- Phần Recommended Products -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="section_title">
+                        <h2><span> <strong>Recommended </strong>Products</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="product_carousel product_column5 owl-carousel">
+                        @forelse ($recommendedProducts as $product)
+                            @php
+                                // Sắp xếp các variant theo giá tăng dần và lấy variant đầu tiên (có giá thấp nhất)
+                                $minVariant = $product->variants->sortBy('price')->first();
+                            @endphp
+                            <div class="single_product">
+                                <div class="product_name">
+                                    <h3>
+                                        <a href="{{ route('client.products.detail', $product->id) }}">
+                                            {{ $product->name }}
+                                        </a>
+                                    </h3>
+                                    <p class="manufacture_product"><a href="#">Accessories</a></p>
+                                </div>
+                                <div class="product_thumb">
+                                    <a class="primary_img" href="{{ route('client.products.detail', $product->id) }}">
+                                        <img src="{{ asset($product->image_primary) }}" alt="">
+                                    </a>
+                                    <div class="label_product">
+                                        @if ($product->discount_percent)
+                                            <span class="label_sale">-{{ $product->discount_percent }}%</span>
+                                        @endif
+                                    </div>
+                                    <div class="action_links">
+                                        <ul>
+                                            <li class="quick_button">
+                                                <a href="#" data-bs-toggle="modal" data-bs-target="#modal_box"
+                                                    title="quick view">
+                                                    <span class="lnr lnr-magnifier"></span>
+                                                </a>
+                                            </li>
+                                            <li class="wishlist">
+                                                <a href="wishlist.html" title="Add to Wishlist">
+                                                    <span class="lnr lnr-heart"></span>
+                                                </a>
+                                            </li>
+                                            <li class="compare">
+                                                <a href="compare.html" title="compare">
+                                                    <span class="lnr lnr-sync"></span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="product_content">
+                                    <div class="product_ratings">
+                                        <ul>
+                                            <li><a href="#"><i class="ion-star"></i></a></li>
+                                            <li><a href="#"><i class="ion-star"></i></a></li>
+                                            <li><a href="#"><i class="ion-star"></i></a></li>
+                                            <li><a href="#"><i class="ion-star"></i></a></li>
+                                            <li><a href="#"><i class="ion-star"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="product_footer d-flex align-items-center">
+                                        <div class="price_box">
+                                            @if ($minVariant)
+                                                <span class="regular_price">
+                                                    {{ $minVariant->price }}
+                                                </span>
+                                            @else
+                                                <span class="regular_price">N/A</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @empty
+                            <p>No recommended products found</p>
+                        @endforelse
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 @endsection
-@section('scripts')
 
+@section('scripts')
+    <!-- Nếu có script riêng cho owl-carousel hoặc xử lý UI, bạn thêm ở đây -->
 @endsection
